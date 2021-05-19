a19a826f1c83b0ff5c4e5d673458200cb5170742
Tâm Trần Đức <19522166@ms.uit.edu.vn>
2021-05-19 19:49:14 +0700

    Fix again

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -8,15 +8,11 @@ namespace Model
 {
     public class Employee
     {
-        public Employee()
-        {
-        }
-
-        public string ID { get; set; }
+        public string Role { get; set; }
+        public string Citizenship { get; set; }
         public string Department { get; set; }
         public string Gender { get; set; }
-        public string Citizenship { get; set; }
-        public string Role { get; set; }
+        public string ID { get; set; }
         public string Name { get; set; }
     }
 }
